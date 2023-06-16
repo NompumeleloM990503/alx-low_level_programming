292b5d21dd824a074dce711c77d8856fb3780426
Nompumelelo <mhlongonompumelelo37@gmail.com>
2023-06-15 18:33:31 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Rturn: Always 0
+ * main - Entry Point
+ * Rturn: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }	
