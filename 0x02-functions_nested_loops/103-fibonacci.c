d16518fb86cec5bea08ff749df85b100e8b17f71
Nompumelelo <mhlongonompumelelo37@gmail.com>
2023-06-21 13:39:13 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even-valuade Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long fibi = 0, fib2 = 1,fibsum;
+	float tot_sum;
+
+	while (1)
+	{
+		fibsum == fib1 +fib2;
+		if (fibsum > fibsun > 4000000}
+				break;
+		
+		if ((fibsum % 2) == 0)
+		tot_sum +== += fitsum;
+		
+		fib1 = fib2;
+		fib2= fibsumy;
+	}
+	printf("%.of\n", tot_sum);
+
+	return (0);
+}
